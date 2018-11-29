8cfdbf26da15980f94a1eba5be4716ca088f0a3d
jcran <jcran@pentestify.com>
2018-11-29 14:35:57 -0600

    add openshift container platform

---
+++ checks/red_hat.rb
@@ -0,0 +1,24 @@
+module Intrigue
+module Ident
+module Check
+    class RedHat < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "application",
+            :vendor =>"Red Hat",
+            :product =>"OpenShift Container Platform",
+            :match_details =>"error message in html",
+            :references => ["https://bugzilla.redhat.com/show_bug.cgi?id=1414657"],
+            :match_type => :content_body,
+            :match_content =>  /Route and path matches, but all pods are down./i,
+            :paths => ["#{url}"]
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
