3c0faba70e2b315e1e12ec125e234003e9019b58
Jonathan Cran <jcran@intrigue.io>
2018-12-12 09:07:18 -0600

    Add COTS tag and tweak regexes for completeness

--- checks/calibre.rb
+++ checks/calibre.rb
@@ -8,16 +8,16 @@ module Check
           {
             :type => "fingerprint",
             :category => "application",
-            :tags => ["EBook Server"],
+            :tags => ["EBook Server","COTS"],
             :vendor =>"Calibre",
             :product =>"Calibre",
             :match_details =>"server header",
             :references => ["https://github.com/kovidgoyal/calibre", 
                             "https://en.wikipedia.org/wiki/Calibre_(software)"],
             :match_type => :content_headers,
-            :match_content =>  /server: calibre/i,
+            :match_content =>  /^server: calibre.*$/i,
             :dynamic_version => lambda { |x|
-              _first_header_capture(x,/server: calibre (.*)/i)
+              _first_header_capture(x,/^server: calibre (.*)$/i)
             },
             :paths => ["#{url}"]
           }
