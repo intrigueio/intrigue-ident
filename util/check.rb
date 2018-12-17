e2cfd644c1a42b8c9318cb9104ce5a6cea0565f9
jcran <jcran@pentestify.com>
2018-12-17 08:55:12 -0600

    dedupe check util output

--- util/check.rb
+++ util/check.rb
@@ -13,7 +13,12 @@ end
 
 puts "Fingerprint: "
 if matches["fingerprint"]
+  uniq_matches = []
   matches["fingerprint"].each do|x|
+    # Make sure not to print dupes
+    next if uniq_matches.include? "#{x["vendor"]} #{x["product"]} #{x["version"]} #{x["update"]}"
+    uniq_matches << "#{x["vendor"]} #{x["product"]} #{x["version"]} #{x["update"]}"
+    # otherwise, print it out 
     puts " - #{x["vendor"]} #{x["product"]} #{x["version"]} #{x["update"]} - #{x["match_details"]} (Tags: #{x["tags"]})"
   end
 end
