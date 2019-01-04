95cf703783e84ea518d04568408197d1dbd63005
jcran <jcran@pentestify.com>
2019-01-04 12:39:03 -0600

    Add check for artifactory

---
+++ checks/artifactory.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+class Artifactory < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Web Server","Embedded"],
+        :vendor => "Artifactory",
+        :product => "Artifactory",
+        :version => nil,
+        :dynamic_version => lambda { |x|
+          _first_header_capture(x,/server: Artifactory\/(.*)$/i) },
+        :match_type => :content_headers,
+        :match_content =>  /server: Artifactory/,
+        :match_details =>"server header",
+        :paths => ["#{url}"]
+      }
+    ]
+  end
+end
+end
+end
+end
