2b8df9fd49b42644dc9e714bfc12dead98d3e513
jcran <jcran@pentestify.com>
2019-01-07 09:46:11 -0600

    add check for juniper ssl vpn

--- checks/juniper.rb
+++ checks/juniper.rb
@@ -12,12 +12,26 @@ module Check
           :vendor =>"Juniper",
           :product =>"Junos Pulse Secure Access Service",
           :match_details => "page title",
-          :match_type => :content_body,
+          :match_type => :content_title,
           :references => [],
-          :match_content =>  /<title>Junos Pulse Secure Access Service/,
+          :match_content =>  /^Junos Pulse Secure Access Service$/,
           :version => nil,
           :paths => ["#{url}"]
-        }
+        },
+        {
+          :type => "fingerprint",
+          :category => "application",
+          :tags => ["Networking", "VPN"],
+          :vendor =>"Juniper",
+          :product =>"Junos Pulse Secure Access Service",
+          :match_details => "unique image link",
+          :match_type => :content_body,
+          :references => [],
+          :match_content =>  /<td background="\/dana-na\/imgs\/footerbg.gif">/,
+          :version => nil,
+          :paths => ["#{url}"]
+        },
+
       ]
     end
 
