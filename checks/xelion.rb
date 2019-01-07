d9c8747121248aa2019c51640258509470ce26c8
jcran <jcran@pentestify.com>
2019-01-06 22:37:59 -0600

    xelion check

---
+++ checks/xelion.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+    class Xelion < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS","Telephony"],
+            :vendor =>"Xelion",
+            :product =>"Phone System",
+            :references => ["https://www.xelion.com/en/"],
+            :match_details =>"title",
+            :references => [""],
+            :match_type => :content_title,
+            :match_content =>  /Xelion Phone System/i,
+            :paths => ["#{url}"]
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
