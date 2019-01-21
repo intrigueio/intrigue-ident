8a4d0d5d46d8d6ca7d63f14b2087d0a0ac9be14a
jcran <jcran@pentestify.com>
2019-01-20 17:07:51 -0800

    gunicorn

---
+++ checks/gunicorn.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+    class Gunicorn < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS","Development"],
+            :vendor => "Gunicorn",
+            :product =>"Gunicorn",
+            :match_details =>"server",
+            :version => nil,
+            :match_type => :content_headers,
+            :match_content =>  /server: gunicorn/i,
+            :dynamic_version => lambda{ |x|
+              _first_header_capture(x,/server: gunicorn\/(.*)/)
+            },
+            :paths => ["#{url}"]
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
