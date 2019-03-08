4ecedf8cfad45e713eb526a7817ff778ee57bdad
jcran <jcran@pentestify.com>
2019-03-07 20:55:44 -0600

    add check for Anquanbao WAF

---
+++ checks/anquanbao.rb
@@ -0,0 +1,30 @@
+module Intrigue
+module Ident
+module Check
+class Anquanbao < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["WAF"],
+        :website => "",
+        :vendor => "Anquanbao",
+        :product => "WAF",
+        :references => [
+          "https://github.com/EnableSecurity/wafw00f"
+        ],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content => /^X-Powered-By-Anquanbao/i,
+        :match_details => "header match",
+        :hide => false,
+        :paths => ["#{url}"]
+      }
+    ]
+  end
+end
+end
+end
+end
