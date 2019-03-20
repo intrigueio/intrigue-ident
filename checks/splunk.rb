0a88a27368f09fbb9364f64b3a5d19da18bd93bb
jcran <jcran@pentestify.com>
2019-03-20 17:53:41 -0500

    add splunk

---
+++ checks/splunk.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+    class Splunk < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS", "Security"],
+            :vendor =>"Splunk",
+            :product =>"Splunk",
+            :match_details =>"server header",
+            :version => nil,
+            :match_type => :content_headers,
+            :match_content => /server: Splunkd$/i,
+            :paths => ["#{url}"],
+            :inference => false
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
