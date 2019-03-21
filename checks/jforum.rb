ff4d915622f0dafb47623fc359a001272e504cc3
jcran <jcran@pentestify.com>
2019-03-20 22:44:07 -0500

    add jforum check

---
+++ checks/jforum.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+    class Jforum < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS", "Development"],
+            :vendor => "Jforum",
+            :product => "Jforum",
+            :match_details => "cookie",
+            :version => "2.x",
+            :match_type => :content_cookies,
+            :match_content =>  /jforum-secure-sso=/i,
+            :paths => ["#{url}"],
+            :inference => false
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
