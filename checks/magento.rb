f87566a877c2abd631195c011709b38522d8c746
Jonathan Cran <jcran@pentestify.com>
2019-03-27 08:00:51 -0500

    add a few more magento checks

--- checks/magento.rb
+++ checks/magento.rb
@@ -17,7 +17,47 @@ module Check
             :match_content =>  /Mage.Cookies.path/i,
             :paths => ["#{url}"], 
             :inference => false
-          }
+          }, 
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS","Payments"],
+            :vendor => "Magento",
+            :product =>"Magento",
+            :match_details =>"Magento",
+            :match_type => :content_headers,
+            :version => nil,
+            :match_content =>  /^x-magento-cache-debug:.*$/i,
+            :paths => ["#{url}"], 
+            :inference => false
+          },
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS","Payments"],
+            :vendor => "Magento",
+            :product =>"Magento",
+            :match_details =>"Magento",
+            :match_type => :content_headers,
+            :version => nil,
+            :match_content =>  /^x-magento-cache-control:.*$/i,
+            :paths => ["#{url}"], 
+            :inference => false
+          },
+          
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["COTS","Payments"],
+            :vendor => "Magento",
+            :product =>"Magento",
+            :match_details =>"Magento",
+            :match_type => :content_headers,
+            :version => nil,
+            :match_content =>  /^x-magento-tags:.*$/i,
+            :paths => ["#{url}"], 
+            :inference => false
+          }          
         ]
       end
 
