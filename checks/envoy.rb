4eb58dcf8869d5dcd603cccaad47e8e8dab6e2c5
Jonathan Cran <jcran@pentestify.com>
2019-04-06 14:25:11 -0500

    add envoy proxy

---
+++ checks/envoy.rb
@@ -0,0 +1,26 @@
+module Intrigue
+module Ident
+module Check
+class Envoy < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "operating_system",
+        :tags => ["Networking","COTS","Wireless"],
+        :vendor => "Envoy",
+        :product => "Envoy",
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content =>  /^server: envoy$/,
+        :match_details =>"server header",
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
