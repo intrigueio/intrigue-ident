1f096051edb0195c0b41436fdca178b07f104237
Jonathan Cran <jcran@pentestify.com>
2019-07-31 20:34:24 -0500

    incapsula miss

--- checks/incapsula.rb
+++ checks/incapsula.rb
@@ -30,7 +30,21 @@ module Check
             :match_content =>  /^x-cdn: Incapsula$/i,
             :paths => ["#{url}"],
             :inference => false
-          }
+          }, 
+          {
+            :type => "fingerprint",
+            :category => "service",
+            :tags => ["Hosting","CDN","WAF"],
+            :vendor =>"Incapsula",
+            :product =>"CDN (Miss)",
+            :match_details =>"incapsula header",
+            :version => nil,
+            :match_type => :content_body,
+            :match_content =>  /Request unsuccessful. Incapsula incident ID/i,
+            :paths => ["#{url}"],
+            :inference => false,
+            :hide => true 
+          } 
         ]
       end
 
