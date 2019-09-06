8de3dbf664ab709b1aa2c3dc7cdf4fb11847b9a1
Jonathan Cran <jcran@pentestify.com>
2019-09-06 08:40:03 -0500

    add a check for fortinet fortigate

---
+++ checks/http/fortinet.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+    class Fortinet < Intrigue::Ident::Check::Base
+
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "service",
+            :tags => ["VPN","Networking"],
+            :vendor => "Fortinet",
+            :product =>"FortiGate",
+            :references => [
+             "https://forum.fortinet.com/tm.aspx?m=130869"
+            ],
+            :match_details =>"server header",
+            :version => nil,
+            :match_type => :content_body,
+            :match_content =>  /FortiToken clock drift detected/i,
+            :paths => ["#{url}"]
+          }
+        ]
+      end
+
+    end
+  end
+  end
+  end
