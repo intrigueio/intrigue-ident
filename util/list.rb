10f4d4d0f10aab265b39e2216ab653d51e8f85fc
Jonathan Cran <jcran@pentestify.com>
2019-09-25 22:34:48 -0500

    add sucuri checks

--- util/list.rb
+++ util/list.rb
@@ -3,7 +3,10 @@ require_relative "../lib/ident"
 include Intrigue::Ident
 
 def list_checks
-  Intrigue::Ident::CheckFactory.checks.map{|x| x.new.generate_checks("[uri]") }.flatten
+  Intrigue::Ident::Http::CheckFactory.checks.map{|x| x.new.generate_checks("[uri]") }.flatten
+  #Intrigue::Ident::Ftp::CheckFactory.checks.map{|x| x.new.generate_checks.flatten
+  #Intrigue::Ident::Smtp::CheckFactory.checks.map{|x| x.new.generate_checks.flatten
+  #Intrigue::Ident::Snmp::CheckFactory.checks.map{|x| x.new.generate_checks.flatten
 end
 
-list_checks.sort_by{|c| "#{c[:type]}" }.each {|c| puts " - #{c[:type]} ... #{c[:name]} #{c[:vendor]} #{c[:product]} #{c[:version]} (Version detection: #{!c[:dynamic_version].nil?}) #{c[:paths]}"}
+list_checks.sort_by{|c| "#{c[:type]}" }.each {|c| puts " - #{c[:type]} ... #{c[:name]} #{c[:vendor]} #{c[:product]} #{c[:version]} (Version detection: #{!c[:dynamic_version].nil?}) #{c[:paths]} ... #{c[:tags]}"}
