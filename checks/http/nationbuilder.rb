aedad92316864ac6d7fe5931dfdc4ef2a92faa61
Jonathan Cran <jcran@pentestify.com>
2019-11-01 19:37:08 -0500

    nationbuilder

---
+++ checks/http/nationbuilder.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+class NationBuilder < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["CMS"],
+        :website => "",
+        :vendor => "NationBuilder",
+        :product => "NationBuilder",
+        :references => [],
+        :version => nil,
+        :match_type => :content_body,
+        :match_content => />Created with NationBuilder<\/a>/i,
+        :match_details => "created by string",
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
