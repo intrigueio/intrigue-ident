1238ea53dd58aa1c14dc10387e937da01bde686a
Jonathan Cran <jcran@pentestify.com>
2020-01-03 13:29:48 -0600

    crowdstrike falcoln

---
+++ checks/http/crowdstrike.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+class Crowdstrike < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["Security", "SaaS"],
+        :vendor => "Crowdstrike",
+        :product => "Falcon",
+        :references => [""],
+        :version => nil,
+        :match_type => :content_cookies,
+        :match_content => /domain\=\.falcon-sandbox\.com/i,
+        :match_details => "Falcon Sandbox cookie",
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
