25c5de6f4c18fc3466f75ff9c44812483bb793c3
Jonathan Cran <jcran@pentestify.com>
2020-02-17 07:44:34 -0600

    add software_ag / nirvana

---
+++ checks/http/software_ag.rb
@@ -0,0 +1,47 @@
+module Intrigue
+  module Ident
+  module Check
+  class SoftwareAg < Intrigue::Ident::Check::Base
+  
+    def generate_checks(url)
+      [
+        {
+          :type => "fingerprint",
+          :category => "application",
+          :tags => ["COTS"],
+          :vendor => "Software AG",
+          :product => "Unified Messaging (Nirvana)",
+          :references => ["http://www.universalmessaging.org/"],
+          :match_details => "server header",
+          :version => nil,
+          :match_type => :content_headers,
+          :match_content =>  /server: my-Channels/i,
+          :paths => ["#{url}"],
+          :inference => false
+        },
+        {
+          :type => "fingerprint",
+          :category => "application",
+          :tags => ["COTS"],
+          :vendor => "Software AG",
+          :product => "Unified Messaging (Nirvana)",
+          :references => ["http://www.universalmessaging.org/"],
+          :match_details => "documentation index version",
+          :version => nil,
+          :match_type => :content_body,
+          :match_content =>  /<h1>Nirvana [\d\.]+ Documentation Index/i,
+          :dynamic_version => lambda{|x| _first_body_capture(x,/<h1>Nirvana ([\d\.]+) Documentation Index/i)},
+          :paths => ["#{url}"],
+          :inference => false
+        }
+      ]
+    end
+
+
+    
+  
+  end
+  end
+  end
+  end
+  
