d27305cbeac83fd22733f323992a69e5349d76ce
Jonathan Cran <jcran@pentestify.com>
2020-02-29 10:00:57 -0600

    webflow missing page

--- checks/http/webflow.rb
+++ checks/http/webflow.rb
@@ -17,7 +17,24 @@ class Webflow < Intrigue::Ident::Check::Base
         :match_content =>  /data-wf-page=\"/i,
         :paths => ["#{url}"],
         :inference => false
+      },
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["Web Framework", "Javascript"],
+        :vendor =>"Webflow",
+        :product =>"Webflow (missing page)",
+        :match_details =>"missing page body string",
+        :references => ["https://webflow.com"],
+        :match_type => :content_body,
+        :match_content =>  /w-section content-wrapper.*The page you are looking for doesn't exist or has been moved./mi,
+        :paths => ["#{url}"],
+        :hide => true,
+        :inference => false
       }
+
+
+      
     ]
   end
 
