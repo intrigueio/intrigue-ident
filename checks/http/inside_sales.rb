800d8abe62f52f9ce61b20c3ef9cdf135b356ce2
Jonathan Cran <jcran@pentestify.com>
2020-03-03 14:41:51 -0600

    insidesales.com

---
+++ checks/http/inside_sales.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+class InsideSales < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["SaaS". "Marketing"],
+        :vendor => "InsideSales.com",
+        :product => "InsideSales.com",
+        :match_details => "server header",
+        :match_type => :content_headers,
+        :version => nil,
+        :match_content =>  /^server: Insidesales.com$/i,
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+
+end
+end
+end
+end
