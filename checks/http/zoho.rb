1aa29ebd6a161723548979c703bdd2a0e88aea74
jen140 <jen140@users.noreply.github.com>
2020-03-12 15:56:02 +0000

    Added "server: ZGS"
    Zoho uses "server: ZGS".
    From the main developers websites we see:
    https://www.zoho.com/
    https://www.zoho.eu/
    https://zohopublic.com/
    
    Sources that have both matches ("ManageEngine ServiceDesk" and "Server: Zoho"):
    https://www.site24x7.com/
    https://www.site24x7.eu/
    
    And some redirects are being done(to accounts.zoho.com), for example:
    http://www.namail.net
    http://precisionaccountingsolutions.com

--- checks/http/zoho.rb
+++ checks/http/zoho.rb
@@ -43,6 +43,21 @@ module Intrigue
           :match_content =>  /ManageEngine AssetExplorer/i,
           :paths => ["#{url}"],
           :inference => false
+        },
+        {
+          :type => "fingerprint",
+          :category => "service",
+          :tags => ["Support"],
+          :vendor =>"Zoho",
+          :product =>"Zoho",
+          :match_details =>"Zoho",
+          :version => nil,
+          :match_type => :content_headers,
+          :match_content =>  /server: ZGS/i,
+          :dynamic_version => lambda { |x| 
+            _first_header_capture(x,/server:.ZGS/i) },
+          :paths => ["#{url}"],
+          :inference => false
         }
       ]
     end
