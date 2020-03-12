7011e37b5bafa3f0d13d591a0a15c63426cf3cb0
jen140 <jen140@users.noreply.github.com>
2020-03-12 17:39:38 +0000

    Added "Micro Focus Business Service Management"
    Looks like the documentation is for clients only, but found a reference on their main support forum:
    https://community.microfocus.com/t5/Application-Perf-Mgmt-BAC-BSM/DPS-Server-console/m-p/322944
    "GW is the frontend and DPS is the backend of BSM."

---
+++ checks/http/bsm.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+class BSM < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["Web Server"],
+        :vendor => "MicroFocus",
+        :product => "BSM",
+        :references => ["https://docs.microfocus.com/SM/9.60/Codeless/Content/integrations/business_service_management/concepts/hp_business_service_management.htm"],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content =>  /server: dps/i,
+        :dynamic_version => lambda { |x| 
+          _first_header_capture(x,/server:.*dps\/([\d\.]*).*/i) },
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
