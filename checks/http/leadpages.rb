7406b6ed35e103bbbbda676ea4c09dcbf2476a88
jen140 <jen140@users.noreply.github.com>
2020-03-12 17:17:48 +0000

    Added Leadpages
    As seen on some webservers:
    https://teamsmiths.com/about/
    https://deco.dance
    https://www.ricardojordao.com
    
    In some cases we have the same icon and in the source code we have the icon in javascript comment and also the message "BUILT WITH LEADPAGES". 
    Also looks like they are all hosted on the same server: 35.202.21.90

---
+++ checks/http/leadpages.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Ident
+module Check
+class Leadpages < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["Hosting", "CDN"],
+        :vendor => "Leadpages",
+        :product => "Leadpages",
+        :references => ["https://www.leadpages.net/"],
+        :version => nil,
+        :match_content =>  /server: leadpages/i,
+        :dynamic_version => lambda { |x| 
+          _first_header_capture(x,/server:.*leadpages/i) },
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
