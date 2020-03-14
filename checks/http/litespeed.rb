8adcfa135b32f03a13cd912b826210b2a4980d08
jen140 <jen140@users.noreply.github.com>
2020-03-14 23:23:29 +0000

    Update litespeed.rb

--- checks/http/litespeed.rb
+++ checks/http/litespeed.rb
@@ -14,9 +14,9 @@ class LiteSpeed < Intrigue::Ident::Check::Base
         :references => ["https://www.litespeedtech.com/products/litespeed-web-server"],
         :version => nil,
         :match_type => :content_headers,
-        :match_content =>  /server: LiteSpeed/,
+        :match_content =>  /^server:\ LiteSpeed$/i,
         :dynamic_version => lambda { |x| 
-          _first_header_capture(x,/server:.LiteSpeed/) },
+          _first_header_capture(x,/^server:\ LiteSpeed$/i) },
         :hide => false,
         :paths => ["#{url}"],
         :inference => false
