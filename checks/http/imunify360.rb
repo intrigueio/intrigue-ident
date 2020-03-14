de870e881cdad588e59d5752b3b9e5a390f56d3a
jen140 <jen140@users.noreply.github.com>
2020-03-14 23:22:38 +0000

    Update imunify360.rb

--- checks/http/imunify360.rb
+++ checks/http/imunify360.rb
@@ -14,9 +14,9 @@ class Imunify360 < Intrigue::Ident::Check::Base
         :references => ["https://www.imunify360.com/blog/webshield-introduction-for-server-administrators"],
         :version => nil,
         :match_type => :content_headers,
-        :match_content =>  /server: imunify360-webshield/i,
+        :match_content =>  /^server:\ imunify360-webshield\/[\d\.]{1,}$/i,
         :dynamic_version => lambda { |x| 
-          _first_header_capture(x,/server:.*imunify360-webshield\/([\d\.]*)/i) },
+          _first_header_capture(x,/^server:\ imunify360-webshield\/[\d\.]{1,}$/i) },
         :paths => ["#{url}"],
         :inference => true
       }
