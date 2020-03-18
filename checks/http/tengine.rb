6bf8c141f6eddd8577d4920a8686679f9bc55b1e
jen140 <jen140@users.noreply.github.com>
2020-03-18 15:01:51 +0000

    Added Tengine(nginx fork)

---
+++ checks/http/tengine.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+class Tengine < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Web Server"],
+        :vendor => "Taobao",
+        :product => "Tengine",
+        :references => ["https://tengine.taobao.org/"],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content =>  /^server:\ Tengine\/{0,1}(\d{1,}\.\d{1,}\.\d{1,}){0,1}|unknown|Aserver$/i,
+        :dynamic_version => lambda { |x| 
+          _first_header_capture(x,/^server:\ Tengine\/{0,1}(\d{1,}\.\d{1,}\.\d{1,}){0,1}|unknown|Aserver$/i) },
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => true
+      }
+    ]
+  end
+end
+end
+end
+end
