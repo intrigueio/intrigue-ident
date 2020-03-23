9fb14f6a50565810e46bfd2ab5dcf92af9a09cc1
jen140 <jen140@users.noreply.github.com>
2020-03-23 16:36:31 +0000

    Added wildfly

---
+++ checks/http/wildfly.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+class Wildfly < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Web Server"],
+        :vendor => "Wildfly",
+        :product => "Wildfly",
+        :references => ["https://wildfly.org/about/"],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content =>  /^server:\ WildFly\/(\d{1,2})$/i,
+        :dynamic_version => lambda { |x|
+          _first_header_capture(x,/^server:\ WildFly\/(\d{1,2})$/i) },
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => true
+      }
+    ]
+  end
+end
+end
+end
+end
