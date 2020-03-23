92516b12d532c6ebfbe94dc9aea028c5e78f350b
jen140 <jen140@users.noreply.github.com>
2020-03-23 18:16:39 +0000

    Added UltraDNS Service

---
+++ checks/http/neustar.rb
@@ -0,0 +1,29 @@
+module Intrigue
+module Ident
+module Check
+class Neustar < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["WAF", "SaaS"],
+        :vendor => "Neustar",
+        :product => "UltraDNS",
+        :references => ["https://www.home.neustar/application-security"],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content =>  /^Server:\ UltraDNS\ Client\ Redirection\ Server$/i,
+        :dynamic_version => lambda { |x|
+          _first_header_capture(x,/^Server:\ UltraDNS\ Client\ Redirection\ Server$/i) },
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => false
+      }
+    ]
+  end
+end
+end
+end
+end
