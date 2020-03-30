534e3c8a1e339c0a0796b503238a2e3302507ead
jen140 <jen140@users.noreply.github.com>
2020-03-30 14:24:47 +0100

    Update parkingcrew.rb

--- checks/http/parkingcrew.rb
+++ checks/http/parkingcrew.rb
@@ -20,6 +20,22 @@ module Intrigue
           :hide => false,
           :paths => ["#{url}"],
           :inference => false
+        },
+        {
+          :type => "fingerprint",
+          :category => "service",
+          :tags => ["Parked"],
+          :vendor => "ParkingCrew",
+          :product => "ParkingCrew",
+          :website => "http://www.parkingcrew.net/",
+          :references => [],
+          :version => nil,
+          :match_type => :content_body,
+          :match_content => /The\ Sponsored\ Listings\ displayed\ above\ are\ served\ automatically\ by\ a\ third\ party/i,
+          :match_details => "The\ Sponsored\ Listings\ displayed\ above\ are\ served\ automatically\ by\ a\ third\ party",
+          :hide => false,
+          :paths => ["#{url}"],
+          :inference => false
         }
       ]
     end
