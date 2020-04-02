395763d7475e15bafe8a7e824f6019dab7788ed7
jen140 <jen140@users.noreply.github.com>
2020-04-02 18:36:48 +0100

    Create ucoz.rb

---
+++ checks/http/ucoz.rb
@@ -0,0 +1,31 @@
+module Intrigue
+  module Ident
+  module Check
+  class uCoz < Intrigue::Ident::Check::Base
+  
+    def generate_checks(url)
+      [
+        {
+          :type => "fingerprint",
+          :category => "service",
+          :tags => ["Hosting"],
+          :vendor => "uCoz",
+          :product => "uCoz",
+          :website => "https://www.ucoz.com/",
+          :references => [],
+          :version => nil,
+          :match_type => :content_body,
+          :match_content => /title\=\"Hosted by uCoz\"/i,
+          :match_details => "title\=\"Hosted by uCoz\"",
+          :hide => false,
+          :paths => ["#{url}"],
+          :inference => false
+        }
+      ]
+    end
+  
+  end
+  end
+  end
+  end
+  
