a49991e7ecaf04ac6c5df98e74ef30758289fb4e
Jonathan Cran <jcran@pentestify.com>
2020-05-03 14:27:24 -0500

    add checks for fresh_service and squarespace

--- checks/http/squarespace.rb
+++ checks/http/squarespace.rb
@@ -17,6 +17,20 @@ class Squarespace < Intrigue::Ident::Check::Base
         :match_content =>  /^Server: Squarespace$/i,
         :paths => ["#{url}"],
         :inference => false
+      },
+      {
+        :type => "fingerprint",
+        :category => "service",
+        :tags => ["SaaS", "CMS"],
+        :vendor => "Squarespace",
+        :product => "Squarespace",
+        :match_details => "squarespace missing page",
+        :version => nil,
+        :match_type => :content_body,
+        :match_content =>  /<h1>400 Bad Request<\/h1>[\n\s]+<p id=\"status-page\">/im,
+        :paths => ["#{url}"],
+        :inference => false,
+        :hide => true
       }
     ]
   end
