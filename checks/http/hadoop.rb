50cff8c2e47e3a9fe60731556fb1bec9e9e4071e
Jonathan Cran <jcran@pentestify.com>
2020-06-18 18:18:47 -0500

    add check for hadoop yarn

---
+++ checks/http/hadoop.rb
@@ -0,0 +1,43 @@
+module Intrigue
+  module Ident
+  module Check
+    class Hadoop < Intrigue::Ident::Check::Base
+  
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["Development", "COTS"],
+            :vendor =>"Hadoop",
+            :product =>"YARN",
+            :match_details =>"unique body string",
+            :version => nil,
+            :match_type => :content_body,
+            :match_content =>  /user\"\>Logged in as: dr\.who\<\/div\>/im,
+            :paths => ["#{url}"],
+            :inference => false
+          },
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["Development", "COTS"],
+            :vendor =>"Hadoop",
+            :product =>"YARN",
+            :match_details =>"unique body string",
+            :version => nil,
+            :match_type => :content_body,
+            :match_content =>  /root.users.dr_dot_who/i,
+            :paths => ["#{url}"],
+            :inference => false
+          }
+
+          
+        ]
+      end
+  
+    end
+  end
+  end
+  end
+  
