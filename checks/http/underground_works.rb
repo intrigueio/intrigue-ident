d6e869fd84c30f2b0ba6dba484d2c06a960a2333
Jonathan Cran <jcran@pentestify.com>
2020-06-29 08:03:41 -0500

    add check for clockwork php debugger

---
+++ checks/http/underground_works.rb
@@ -0,0 +1,31 @@
+module Intrigue
+module Ident
+module Check
+class UndergroundWorks < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Development"],
+        :vendor => "Underground Works",
+        :website => "https://underground.works/clockwork/",
+        :product => "Clockwork",
+        :references => ["https://github.com/itsgoingd/clockwork"],
+        :version => nil,
+        :match_type => :content_headers,
+        :match_content => /X-Clockwork-Id:.*$/i,
+        :match_details => "unique header match",
+        :hide => false,
+        :paths => ["#{url}"],
+        :inference => false,
+        :issue => "exposed_debugging_interface"
+      }
+    ]
+  end
+
+end
+end
+end
+end
