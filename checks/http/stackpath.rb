71c81a5650faddb6fe51e6190a7b48e6ec21ae76
Duarte Mateus <duartereismateus@gmail.com>
2020-08-24 14:17:11 -0700

    tighten stackpath.

--- checks/http/stackpath.rb
+++ checks/http/stackpath.rb
@@ -14,9 +14,9 @@ class StackPath < Intrigue::Ident::Check::Base
         :references => ["https://www.stackpath.com/products/cdn/"],
         :version => nil,
         :match_type => :content_headers,
-        :match_content =>  /^server:\ NetDNA-cache\/[\d\.]{1,}$/,
+        :match_content =>  /^server:\ NetDNA-cache\/(\d+(\.\d+)*)$/,
         :dynamic_version => lambda { |x| 
-          _first_header_capture(x,/^server:\ NetDNA-cache\/[\d\.]{1,}$/i) },
+          _first_header_capture(x,/^server:\ NetDNA-cache\/(\d+(\.\d+)*)$/i) },
         :hide => false,
         :paths => ["#{url}"],
         :inference => true
