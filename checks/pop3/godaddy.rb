96f85a37464695c919420472ac464572a9c9c3b6
Jonathan Cran <jcran@pentestify.com>
2020-09-09 16:13:54 -0500

    fixes #50

---
+++ checks/pop3/godaddy.rb
@@ -0,0 +1,27 @@
+module Intrigue
+  module Ident
+  module Pop3Check
+  class Godaddy < Intrigue::Ident::Pop3Check::Base
+    def generate_checks
+      [
+        {
+          :type => "fingerprint",
+          :category => "service",
+          :tags => ["Pop3 Server"],
+          :vendor => "Godaddy",
+          :product => "Pop Service (Dovecot)",
+          :references => [],
+          :version => nil,
+          :match_type => :content_banner,
+          :match_content => /^\+OK Fenix ready\.\r\n$/,
+          :match_details => "godaddy defaults to this banner",
+          :hide => false,
+          :inference => false
+        }
+      ]
+    end
+  end
+  end
+  end
+  end
+  
