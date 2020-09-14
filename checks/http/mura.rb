d37a1de89fc1fe3a69c56063e2dc5617cbf0e3e2
Duarte <duartemateus@outlook.com>
2020-09-14 13:22:28 +0100

    Fix for mura.

--- checks/http/mura.rb
+++ checks/http/mura.rb
@@ -1,30 +1,30 @@
 module Intrigue
-module Ident
-module Check
-class AlmubdaCMS < Intrigue::Ident::Check::Base
-
-  def generate_checks(url)
-    [
-      {
-        :type => "fingerprint",
-        :category => "application",
-        :tags => ["CMS"],
-        :vendor => "Mura",
-        :product => "Mura",
-        :references => [],
-        :version => nil,
-        :match_type => :content_body,
-        :match_content => /Mura CMS ([\d\.]+)/i,
-        :dynamic_version => lambda { |x| _first_body_capture(x, /Mura CMS ([\d\.]+)/i)},
-        :match_details => "Header match",
-        :hide => false,
-        :paths => ["#{url}"],
-        :inference => true
-      }
-    ]
+  module Ident
+    module Check
+      class AlmubdaCMS < Intrigue::Ident::Check::Base
+        def generate_checks(url)
+          [
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["CMS"],
+              :vendor => "Mura",
+              :product => "Mura",
+              :website => "http://www.getmura.com/",
+              :match_details => "Mura CMS - generator tag page reference",
+              :version => nil,
+              :match_type => :content_body,
+              :match_content => /<meta name="generator" content="Mura CMS/i,
+              :dynamic_version => lambda { |x|
+                _first_body_capture(x, /<meta name="generator" content="Mura CMS (\d+(\.\d+)*)/i)
+              },
+              :hide => false,
+              :paths => ["#{url}"],
+              :inference => true,
+            },
+          ]
+        end
+      end
+    end
   end
-
-end
-end
-end
 end
