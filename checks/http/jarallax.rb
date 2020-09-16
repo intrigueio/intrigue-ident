8bfc30ec7d67449fbe729e953111e80688bb21e7
Jonathan Cran <jcran@pentestify.com>
2020-09-16 17:15:55 -0500

    fork excon, use our own user agent, add checks for eforum, jarallax, skrollr

---
+++ checks/http/jarallax.rb
@@ -0,0 +1,28 @@
+module Intrigue
+module Ident
+module Check
+class Jarallax < Intrigue::Ident::Check::Base
+
+  def generate_checks(url)
+    [
+      {
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Javascript"],
+        :vendor => "Jarallax",
+        :product => "Jarallax",
+        :website => "https://github.com/nk-o/jarallax",
+        :match_type => :content_body,
+        :match_content => /data-jarallax-video\'/i,
+        :match_details => "javascript class load",
+        :paths => [ { :path  => "#{url}", :follow_redirects => true } ]
+      }
+    ]
+  end
+
+end
+end
+end
+end
+
+
