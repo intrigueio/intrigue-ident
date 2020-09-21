f7534c018c5e18faa62bc2d4499bb74a2a29c765
Xiao <thechowdud@gmail.com>
2020-09-21 17:39:58 +0100

    Updates External Checks to include in util/ident.rb
    Updates the External Checks to include the Drupal folder (containing plugins) in the util/ident.rb.

--- util/ident.rb
+++ util/ident.rb
@@ -251,6 +251,7 @@ def main
   if opts[:include]
     # follow directory structure from ident
     checks = Dir.glob("#{opts[:include]}/checks/*/*.rb")
+    checks += Dir.glob("#{opts[:include]}/checks/http/drupal/*.rb")
     checks += Dir.glob("#{opts[:include]}/checks/http/joomla/*.rb")
     checks += Dir.glob("#{opts[:include]}/checks/http/wordpress/*.rb")
     puts "Requiring #{checks.count} files from include path: #{opts[:include]}" if opts[:debug]
