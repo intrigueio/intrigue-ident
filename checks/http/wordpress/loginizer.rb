3afadbb3f60fd7bf04e739ae314a5e9e80953842
Shpend Kurtishaj <shpendk13@gmail.com>
2020-10-22 23:55:54 +0200

    fingerprint for wordpress loginizer plugin

---
+++ checks/http/wordpress/loginizer.rb
@@ -0,0 +1,31 @@
+module Intrigue
+    module Ident
+    module Check
+    class Loginizer < Intrigue::Ident::Check::Base
+    
+      def generate_checks(url)
+        [
+          {
+            :type => "fingerprint",
+            :category => "application",
+            :tags => ["Wordpress Plugin"],
+            :vendor =>"Wordpress",
+            :product =>"Loginizer",
+            :match_details =>"readme file",
+            :match_type => :content_body,
+            :match_content =>  /=== Loginizer ===/i,
+            :dynamic_version => lambda { |x|
+              _first_body_capture(x,/Stable tag\: (\d+\.\d+\.\d+)/i)
+            },
+            :paths => [ { :path  => "#{url}/wp-content/plugins/loginizer/readme.txt", :follow_redirects => true } ],
+            :require_product => "Wordpress",
+            :inference => false
+          }
+        ]
+      end
+    
+    end
+    end
+    end
+    end
+    
