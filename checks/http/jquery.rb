7c1196b09962cd324a2f72127557c1cf3f21838c
Duarte <duartemateus@outlook.com>
2020-12-04 20:40:06 +0000

    Jquery check fix. Min version was not matching. Also added more descriptive messages on the match and added in website.

--- checks/http/jquery.rb
+++ checks/http/jquery.rb
@@ -1,72 +1,71 @@
 module Intrigue
-module Ident
-module Check
-class Jquery < Intrigue::Ident::Check::Base
-
-  def generate_checks(url)
-    [
-      {
-        :type => "fingerprint",
-        :category => "application",
-        :tags => ["Javascript"],
-        :vendor => "JQuery",
-        :product =>"JQuery",
-        :match_details =>"version in js file",
-        :match_type => :content_body,
-        :match_content =>  /jQuery v\d\.\d\.\d \| \(c\) JS Foundation and other contributors/i,
-        :dynamic_version => lambda {|x| 
-          _first_body_capture(x,/jQuery v(\d\.\d\.\d) \| \(c\) JS Foundation and other contributors/i) },
-        :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => true
-      },
-      {
-        :type => "fingerprint",
-        :category => "application",
-        :tags => ["Javascript"],
-        :vendor => "JQuery",
-        :product =>"JQuery",
-        :match_details =>"filename",
-        :match_type => :content_body,
-        :version => nil,
-        :references => [],
-        :match_content =>  /script\ src=[\"|\']https:\/\/code\.jquery\.com\/jquery-/i,
-        :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => false
-      },
-      {
-        :type => "fingerprint",
-        :category => "application",
-        :tags => ["Javascript"],
-        :vendor => "JQuery",
-        :product =>"Datatables",
-        :match_details =>"filename",
-        :match_type => :content_body,
-        :version => nil,
-        :references => [],
-        :match_content =>  /jquery\.dataTables\.min\.js[\"|\']\>/i,
-        :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => false
-      },
-      {
-        :type => "fingerprint",
-        :category => "application",
-        :tags => ["Javascript"],
-        :vendor => "JQuery",
-        :product =>"JQuery",
-        :match_details =>"filename",
-        :match_type => :content_body,
-        :version => nil,
-        :references => [],
-        :match_content =>  /src=.*?\/jquery-([\d\.]+).js\"\>/i,
-        :dynamic_version => lambda { |x| _first_body_capture(x, /src=.*?\/jquery-([\d\.]+).js\"\>/i)},
-        :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => true
-      }
-      
-    ]
+  module Ident
+    module Check
+      class Jquery < Intrigue::Ident::Check::Base
+        def generate_checks(url)
+          [
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["Javascript"],
+              :website => "https://jquery.com/",
+              :vendor => "JQuery",
+              :product => "JQuery",
+              :match_details => "version in js file",
+              :match_type => :content_body,
+              :match_content => /jQuery v(\d+(\.\d+)*) \| \(c\) JS Foundation and other contributors/i,
+              :dynamic_version => lambda { |x|
+                _first_body_capture(x, /jQuery v(\d+(\.\d+)*) \| \(c\) JS Foundation and other contributors/i)
+              },
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => true,
+            },
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["Javascript"],
+              :website => "https://jquery.com/",
+              :vendor => "JQuery",
+              :product => "JQuery",
+              :match_details => "script page referece",
+              :match_type => :content_body,
+              :version => nil,
+              :match_content => /script\ src=[\"|\']https:\/\/code\.jquery\.com\/jquery-/i,
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => false,
+            },
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["Javascript"],
+              :website => "https://jquery.com/",
+              :vendor => "JQuery",
+              :product => "Datatables",
+              :match_details => "script page referece",
+              :match_type => :content_body,
+              :version => nil,
+              :match_content => /jquery\.dataTables\.min\.js[\"|\']\>/i,
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => false,
+            },
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["Javascript"],
+              :website => "https://jquery.com/",
+              :vendor => "JQuery",
+              :product => "JQuery",
+              :match_details => "script page referece",
+              :match_type => :content_body,
+              :version => nil,
+              :match_content => /src=.*?\/jquery-(\d+(\.\d+)*)(?:\.min)?.js/i,
+              :dynamic_version => lambda { |x| _first_body_capture(x, /src=.*?\/jquery-(\d+(\.\d+)*)(?:\.min)?.js/i) },
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => true,
+            },
+          ]
+        end
+      end
+    end
   end
-
-end
-end
-end
 end
