e53d3d43bca449ba6880013c12bb4d3a14855f26
Duarte <duartemateus@outlook.com>
2020-12-30 20:20:31 +0000

    expand moodle session cookie match.

--- checks/http/moodle.rb
+++ checks/http/moodle.rb
@@ -1,46 +1,43 @@
 module Intrigue
   module Ident
-  module Check
-    class Moodle < Intrigue::Ident::Check::Base
-  
-      def generate_checks(url)
-        [
-          {
-            :type => "fingerprint",
-            :category => "application",
-            :tags => ["CMS"],
-            :vendor =>"Moodle",
-            :product =>"Moodle",
-            :match_details =>"cookie",
-            :version => nil,
-            :match_type => :content_cookies,
-            :match_content =>  /MoodleSession=/i,
-            :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-            :inference => false
-          },
-          {
-            :type => "fingerprint",
-            :category => "application",
-            :tags => ["CMS"],
-            :vendor =>"Moodle",
-            :product =>"Moodle",
-            :match_details =>"Version detection via /lib/upgrade.txt",
-            :version => nil,
-            :match_type => :content_body,
-            :match_content =>  /This files describes API changes in core libraries and APIs/i,
-            :paths => [ { :path  => "#{url}/lib/upgrade.txt", :follow_redirects => true } ],
-            :inference => true,
-            :require_product => "Moodle",
-            :dynamic_version => lambda {
-              |x|
-              _first_body_capture(x, /\=\=\= (\d+(\.\d+)*) \=\=\=/i)
+    module Check
+      class Moodle < Intrigue::Ident::Check::Base
+        def generate_checks(url)
+          [
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["CMS"],
+              :vendor => "Moodle",
+              :product => "Moodle",
+              :match_details => "Moodle - MoodleSession cookie",
+              :version => nil,
+              :match_type => :content_cookies,
+              :match_content => /MoodleSession(prod)?=/i,
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => false,
             },
-          }
-        ]
+            {
+              :type => "fingerprint",
+              :category => "application",
+              :tags => ["CMS"],
+              :vendor => "Moodle",
+              :product => "Moodle",
+              :match_details => "Version detection via /lib/upgrade.txt",
+              :version => nil,
+              :match_type => :content_body,
+              :match_content => /This files describes API changes in core libraries and APIs/i,
+              :paths => [{ :path => "#{url}/lib/upgrade.txt", :follow_redirects => true }],
+              :inference => true,
+              :require_product => "Moodle",
+              :dynamic_version => lambda {
+                |x|
+                _first_body_capture(x, /\=\=\= (\d+(\.\d+)*) \=\=\=/i)
+              },
+            },
+          ]
+        end
       end
-  
     end
   end
-  end
-  end
-  
+end
