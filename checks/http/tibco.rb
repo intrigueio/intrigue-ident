929e66691546767a478f40559e48ccd90dcf2cb9
jcran <jcran@intrigue.io>
2021-02-01 19:10:41 -0600

    add jaspersoft paths, improve check

--- checks/http/tibco.rb
+++ checks/http/tibco.rb
@@ -11,32 +11,64 @@ class Tibco < Intrigue::Ident::Check::Base
         :tags => ["Development"],
         :vendor =>"TIBCO",
         :product =>"JasperReports Server",
-        :match_details =>"location header (Professional Edition)",
+        :match_details =>"login page (Professional Edition)",
         :version => nil,
-        :match_type => :content_title,
-        :match_content => /JasperServer/i,
+        :match_type => :content_body,
+        :match_content => /loginPage/i,
         :paths => [{ :path => "#{url}/jasperserver/login.html", :follow_redirects => true } ],
         :require_product => "HTTP Server", # require apache 
-        :inference => false
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
       }, 
       { # check for jaasperreports when you see nginx
         :type => "fingerprint",
         :category => "application",
-        :tags => ["Development"],
+        :tags => ["Development","Panel"],
         :vendor =>"TIBCO",
         :product =>"JasperReports Server",
-        :match_details =>"location header (Professional Edition)",
+        :match_details =>"login page (Professional Edition)",
         :version => nil,
-        :match_type => :content_title,
-        :match_content => /JasperServer/i,
+        :match_type => :content_body,
+        :match_content => /loginPage/i,
         :paths => [{ :path => "#{url}/jasperserver/login.html", :follow_redirects => true } ],
         :require_product => "Nginx", # require nginx 
-        :inference => false
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
+      }, 
+      { # check for jaasperreports PRO when you see apache
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Development","Panel"],
+        :vendor =>"TIBCO",
+        :product =>"JasperReports Server",
+        :match_details =>"login page (Professional Edition)",
+        :version => nil,
+        :match_type => :content_body,
+        :match_content => /loginPage/i,
+        :paths => [{ :path => "#{url}/jasperserver-pro/login.html", :follow_redirects => true } ],
+        :require_product => "HTTP Server", # require apache 
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
+      }, 
+      { # check for jaasperreports PRO when you see nginx
+        :type => "fingerprint",
+        :category => "application",
+        :tags => ["Development","Panel"],
+        :vendor =>"TIBCO",
+        :product =>"JasperReports Server",
+        :match_details =>"login page (Professional Edition)",
+        :version => nil,
+        :match_type => :content_body,
+        :match_content => /loginPage/i,
+        :paths => [{ :path => "#{url}/jasperserver-pro/login.html", :follow_redirects => true } ],
+        :require_product => "Nginx", # require nginx 
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
       }, 
       {
         :type => "fingerprint",
         :category => "application",
-        :tags => ["Development"],
+        :tags => ["Development","Panel"],
         :vendor =>"TIBCO",
         :product =>"JasperReports Server",
         :match_details =>"location header (Professional Edition)",
@@ -44,7 +76,8 @@ class Tibco < Intrigue::Ident::Check::Base
         :match_type => :content_headers,
         :match_content => /^Location: \/jasperserver-pro$/i,
         :paths => [ { :path  => "#{url}", :follow_redirects => false } ],
-        :inference => false
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
       },
       {
         :type => "fingerprint",
@@ -57,7 +90,8 @@ class Tibco < Intrigue::Ident::Check::Base
         :match_type => :content_body,
         :match_content => /src=\"\/jasperserver\/scripts\/jasperserver\.js/i,
         :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => false
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
       },
       {
         :type => "fingerprint",
@@ -65,12 +99,13 @@ class Tibco < Intrigue::Ident::Check::Base
         :tags => ["Development"],
         :vendor =>"TIBCO",
         :product =>"JasperReports Server",
-        :match_details =>"location header (Professional Edition)",
+        :match_details =>"jasperserver-pro cookie",
         :version => nil,
         :match_type => :content_cookies,
         :match_content => /Path=\/jasperserver-pro\//i,
         :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => false
+        :inference => false,
+        :issues => ["exposed_jaspersoft_panel"]
       }, 
       {
         :type => "fingerprint",
