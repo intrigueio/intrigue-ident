84fc531e59f24927cd6746a9005379c27f1feba4
Shpend Kurtishaj <shpendk13@gmail.com>
2021-02-16 21:14:46 +0100

    support two and three digit versions

--- checks/http/centreon.rb
+++ checks/http/centreon.rb
@@ -15,7 +15,7 @@ module Intrigue
             :version => nil,
             :match_type => :content_body,
             :match_content =>  /mailto:contact@centreon.com/i,
-            :dynamic_version => lambda{|x| _first_body_capture(x,/v. (\d\.\d\.\d)/) },
+            :dynamic_version => lambda{|x| _first_body_capture(x,/v. (\d{0,3}\.\d{0,3}\.\d{0,3})/) },
             :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
             :inference => true
           }
