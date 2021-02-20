ec2bff9b046af13749573f004e07577b159198d3
Duarte <duartemateus@outlook.com>
2021-02-20 23:12:57 +0000

    Fixes checks, changes to Ident, Changes to ssh Commented out recog from being called when doing SSH checks. Fix for imunify360 Fix for sap.

--- checks/http/imunify360.rb
+++ checks/http/imunify360.rb
@@ -1,28 +1,28 @@
 module Intrigue
-module Ident
-module Check
-class Imunify360 < Intrigue::Ident::Check::Base
-
-  def generate_checks(url)
-    [
-      {
-        :type => "fingerprint",
-        :category => "service",
-        :tags => ["WebServer","Proxy Server"],
-        :vendor => "CloudLinux",
-        :product => "Imunify360",
-        :references => ["https://www.imunify360.com/blog/webshield-introduction-for-server-administrators"],
-        :version => nil,
-        :match_type => :content_headers,
-        :match_content =>  /^server:\ imunify360-webshield\/[\d\.]{1,}$/i,
-        :dynamic_version => lambda { |x| 
-          _first_header_capture(x,/^server:\ imunify360-webshield\/[\d\.]{1,}$/i) },
-        :paths => [ { :path  => "#{url}", :follow_redirects => true } ],
-        :inference => true
-      }
-    ]
+  module Ident
+    module Check
+      class Imunify360 < Intrigue::Ident::Check::Base
+        def generate_checks(url)
+          [
+            {
+              :type => "fingerprint",
+              :category => "service",
+              :tags => ["WebServer", "Proxy Server"],
+              :vendor => "CloudLinux",
+              :product => "Imunify360",
+              :references => ["https://www.imunify360.com/blog/webshield-introduction-for-server-administrators"],
+              :version => nil,
+              :match_type => :content_headers,
+              :match_content => /^server:\ imunify360-webshield/i,
+              :dynamic_version => lambda { |x|
+                _first_header_capture(x, /^server:\ imunify360-webshield\/(\d+(\.\d+)*)/i)
+              },
+              :paths => [{ :path => "#{url}", :follow_redirects => true }],
+              :inference => true,
+            },
+          ]
+        end
+      end
+    end
   end
-end
-end
-end
 end
