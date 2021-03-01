217015e91449f595aa8a4b531dfe963a93c1c944
Florian Pfitzer <florian@pfitzer.me>
2021-03-01 10:21:38 +0100

    distinguish between webmin and usermin

--- checks/http/webmin.rb
+++ checks/http/webmin.rb
@@ -16,8 +16,13 @@ class Webmin < Intrigue::Ident::Check::Base
         references: [],
         match_content: /server: MiniServ/i,
         version: nil,
-        dynamic_version: lambda { |x| 
-          _first_header_capture(x,/server: MiniServ\/(.*)/i)},
+        dynamic_version: lambda { |x|
+          _first_header_capture(x,/server: MiniServ\/(.*)/i)
+        },
+        dynamic_hide: lambda{ |x|
+          # hide if it is usermin and not webmin
+          return true if _first_body_match(x,/<title>Login to Usermin/)
+        },
         paths: [ { path: "#{url}", follow_redirects: true } ],
         inference: true
       },
@@ -35,7 +40,26 @@ class Webmin < Intrigue::Ident::Check::Base
         paths: [ { path: "#{url}", follow_redirects: true } ],
         inference: false,
         issue: "exposed_admin_panel_unauthenticated"
-      }
+      },
+      {
+        type: "fingerprint",
+        category: "application",
+        tags: ["Administrative"],
+        vendor: "Usermin",
+        product: "Usermin",
+        description: "page title",
+        match_type: :content_body,
+        references: [],
+        match_content: /<title>Login to Usermin/,
+        version: nil,
+        paths: [ { path: "#{url}", follow_redirects: true } ],
+        inference: false,
+        dynamic_version: lambda { |x|
+          _first_header_capture(x,/server: MiniServ\/(.*)/i)
+        },
+        issue: "exposed_admin_panel_unauthenticated"
+      },
+
     ]
   end
 
