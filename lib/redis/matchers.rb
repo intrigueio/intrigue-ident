3890086fd5bfba4f7c07154c017d59c2564cb6c3
Duarte <duartemateus@outlook.com>
2021-03-07 00:56:44 +0000

    Changes to redis matcher

--- lib/redis/matchers.rb
+++ lib/redis/matchers.rb
@@ -1,29 +1,63 @@
 module Intrigue
-module Ident
-module Redis
-module Matchers
+  module Ident
+    module Redis
+      module Matchers
+        require_relative "redis"
+        include Intrigue::Ident::Redis
 
-  require_relative 'redis'
-  include Intrigue::Ident::Redis
-  
-  require_relative 'content'
-  include Intrigue::Ident::Redis::Content
+        require_relative "content"
+        include Intrigue::Ident::Redis::Content
 
-  def match_redis_response_hash(check,response_hash)
-    
-    if check[:type] == "fingerprint"
-      if check[:match_type] == :content_os_version
-        match = _construct_match_response(check,response_hash) if _os_version(response_hash) =~ check[:match_content]
-      elsif check[:match_type] == :content_redis_version
-        match = _construct_match_response(check,response_hash) if _redis_version(response_hash) =~ check[:match_content]
+        def match_redis_response_hash(check, response_hash)
+          if check[:type] == "fingerprint"
+            unless check[:matches] #
+              check[:matches] = [
+                {
+                  match_type: check[:match_type],
+                  match_content: check[:match_content],
+                },
+              ]
+            end
+            match_results = []
+
+            # stick it in our array, so we can keep track of each individual match
+            check[:matches].each do |m|
+
+              ###
+              ### Different check types require differnt check methods,
+              ### this handles that
+              ###
+              if m[:match_type] == :content_os_version
+                value = _os_version(response_hash) =~ m[:match_content] ? true : false
+              elsif m[:match_type] == :content_redis_version
+                value = _redis_version(response_hash) =~ m[:match_content] ? true : false
+                # stick it in our array, so we can keep track of each individual match
+                match_results << value
+              end
+
+              #
+              # Now, we need to apply our logic about what a match actually is
+              # before we can return the right thing
+              #
+              # valid values:
+              # - :any
+              # - :all
+              #
+              out = nil
+              if check[:match_logic] == :any && match_results.any?
+                out = _construct_match_response(check, response_hash)
+              elsif check[:match_logic] == :all && match_results.all?
+                out = _construct_match_response(check, response_hash)
+                # legacy, default to simply returning if all (can only be one)
+              elsif !check[:match_logic] && match_results.all?
+                out = _construct_match_response(check, response_hash)
+              end
+            end
+          elsif check[:type] == "content"
+            match = _construct_match_response(check, response_hash)
+          end
+        end
       end
-    elsif check[:type] == "content"
-      match = _construct_match_response(check,response_hash)
     end
-
   end
-
-end
-end
-end
 end
