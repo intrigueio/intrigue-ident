287513dd4fe80d2ac7c89a75754f956d56fae5c6
user <user@pop-os.localdomain>
2021-03-31 04:21:24 -0500

    Adds cambium ePMP 3000 snmp check

---
+++ checks/snmp/cambium.rb
@@ -0,0 +1,29 @@
+module Intrigue
+    module Ident
+    module SnmpCheck
+    class Cambium < Intrigue::Ident::SnmpCheck::Base
+    
+      def generate_checks 
+        [
+          {
+            type: "fingerprint",
+            category: "application",
+            tags: ["SNMPServer", "Networking"],
+            vendor: "Cambium",
+            product: "ePMP 3000 AP",
+            references: [],
+            version: nil,
+            match_type: :content_banner,
+            match_content: /Linux ePMP 3000 AP-VBN-CBM/i,
+            dynamic_version: lambda { |x| _first_banner_capture(x, /(\d\.\d\.\d\.\d\-?\w*)/i)},
+            description: "snmp banner",
+            hide: false,
+            inference: false
+          }
+        ]
+      end
+    end
+    end
+    end
+    end
+    
