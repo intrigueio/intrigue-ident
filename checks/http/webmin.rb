c318e892f39eccd3dc9d76cce7d6517832c9c52b
Shpend Kurtishaj <shpendk13@gmail.com>
2021-04-02 22:20:38 +0200

    test for usermin/webmin based on page title

--- checks/http/webmin.rb
+++ checks/http/webmin.rb
@@ -9,35 +9,16 @@ class Webmin < Intrigue::Ident::Check::Base
         type: "fingerprint",
         category: "application",
         tags: ["Administrative"],
-        vendor:"Webmin",
-        product:"Webmin",
-        description: "miniserv server",
-        match_type: :content_headers,
-        references: [],
-        match_content: /server: MiniServ/i,
+        vendor: "Webmin",
+        product: "Webmin",
+        description: "page title",
+        match_type: :content_body,
+        match_content: /<title>Login to Webmin/,
         version: nil,
         dynamic_version: lambda { |x|
           _first_header_capture(x,/server: MiniServ\/(.*)/i)
         },
-        dynamic_hide: lambda{ |x|
-          # hide if it is usermin and not webmin
-          return true if _first_body_match(x,/<title>Login to Usermin/)
-        },
-        paths: [ { path: "#{url}", follow_redirects: true } ],
-        inference: true
-      },
-      {
-        type: "fingerprint",
-        category: "application",
-        tags: ["Administrative"],
-        vendor:"Webmin",
-        product:"Webmin",
-        description: "page title",
-        match_type: :content_body,
-        references: [],
-        match_content: /<title>Login to Webmin/,
-        version: nil,
-        paths: [ { path: "#{url}", follow_redirects: true } ],
+        paths: [{ path: "#{url}", follow_redirects: true }],
         inference: false,
         issue: "exposed_admin_panel_unauthenticated"
       },
@@ -49,14 +30,13 @@ class Webmin < Intrigue::Ident::Check::Base
         product: "Usermin",
         description: "page title",
         match_type: :content_body,
-        references: [],
         match_content: /<title>Login to Usermin/,
         version: nil,
-        paths: [ { path: "#{url}", follow_redirects: true } ],
-        inference: false,
         dynamic_version: lambda { |x|
           _first_header_capture(x,/server: MiniServ\/(.*)/i)
         },
+        paths: [{ path: "#{url}", follow_redirects: true }],
+        inference: false,
         issue: "exposed_admin_panel_unauthenticated"
       },
 
