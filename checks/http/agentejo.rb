8181f61ad3828d9100547e9d059fb331db448d14
Shpend Kurtishaj <shpendk13@gmail.com>
2021-04-14 23:24:24 +0200

    update to correct vendor

--- checks/http/agentejo.rb
+++ checks/http/agentejo.rb
@@ -2,17 +2,17 @@ module Intrigue
   module Ident
     module Check
       module Private
-        class CockpitCms < Intrigue::Ident::Check::Base
+        class Agentejo < Intrigue::Ident::Check::Base
           def generate_checks(url)
             [
               {
                 type: "fingerprint",
                 category: "application",
                 tags: ["CMS",],
-                vendor: "Cockpit",
-                product: "CMS",
+                vendor: "Agentejo",
+                product: "Cockpit",
                 website: "https://getcockpit.com/",
-                description: "Cockpit CMS",
+                description: "Agentejo Cockpit CMS",
                 version: nil,
                 match_type: :content_title,
                 match_content: /^Authenticate Please!$/i,
