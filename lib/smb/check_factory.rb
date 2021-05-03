adeed989e57db99cd078a410f2c88b7aa4baa5ad
Duarte <duarte.mateus@intrigue.io>
2021-05-03 13:56:22 +0100

    removes commented out code.

--- lib/smb/check_factory.rb
+++ lib/smb/check_factory.rb
@@ -2,25 +2,9 @@ module Intrigue
   module Ident
     module Smb
       class CheckFactory
-        #
-        # Register a new handler
-        #
-
         def self.inherited(base)
           Intrigue::Ident::Base::CheckFactory.register(base)
         end
-
-        # def self.register(klass)
-        #   @checks ||= []
-        #   @checks << klass if klass
-        # end
-
-        # #
-        # # Provide the full list of checks
-        # #
-        # def self.checks
-        #   @checks || []
-        # end
       end
     end
   end
