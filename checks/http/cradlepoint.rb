2497a3ba83c80e861d9d85929398569022f4e6b8
Duarte <duarte.mateus@intrigue.io>
2021-05-04 20:56:47 +0100

    Adds missing website and reference to Cradlepoint.

--- checks/http/cradlepoint.rb
+++ checks/http/cradlepoint.rb
@@ -1,30 +1,30 @@
 module Intrigue
-module Ident
-module Check
-class Cerberus < Intrigue::Ident::Check::Base
-
-  def generate_checks(url)
-    [
-      {
-        type: "fingerprint",
-        category: "application",
-        tags: ["WebServer"],
-        vendor: "Cradlepoint",
-        product:"HTTP Service",
-        description:"server header",
-        version: nil,
-        match_type: :content_headers,
-        match_content:  /^server:.*Cerberus.*$/,
-        dynamic_version: lambda{ |x| 
-          _first_header_capture(x,/^server:.*Cerberus\/([\d\.]*)\s.*$/i) 
-        },
-        paths: [ { path: "#{url}", follow_redirects: true } ],
-        inference: true
-      }
-    ]
+  module Ident
+    module Check
+      class Cradlepoint < Intrigue::Ident::Check::Base
+        def generate_checks(url)
+          [
+            {
+              type: 'fingerprint',
+              category: 'application',
+              tags: ['WebServer'],
+              vendor: 'Cradlepoint',
+              product: 'HTTP Service',
+              website: 'https://cradlepoint.com/',
+              references: ['https://cradlepoint.com/vulnerability-alerts/'],
+              description: 'server header',
+              version: nil,
+              match_type: :content_headers,
+              match_content: /^server:.*Cerberus.*$/,
+              dynamic_version: lambda { |x|
+                _first_header_capture(x, %r{^server:.*Cerberus/([\d.]*)\s.*$}i)
+              },
+              paths: [{ path: url.to_s, follow_redirects: true }],
+              inference: true
+            }
+          ]
+        end
+      end
+    end
   end
-
-end
-end
-end
 end
