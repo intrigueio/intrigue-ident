5eb46c31bfc3293eefa2f987fb617bd348daabc8
Adam Bakalar <bakalar94@gmail.com>
2021-06-21 22:26:02 +0200

    Added IP check for apache zookeeper, formatted other IP checks

--- checks/ip/apache.rb
+++ checks/ip/apache.rb
@@ -1,24 +1,27 @@
 module Intrigue
   module Ident
     module IpCheck
-      class Memcached < Intrigue::Ident::IpCheck::Base
+      class Apache < Intrigue::Ident::IpCheck::Base
         def generate_checks
           [
             {
               type: 'fingerprint',
               category: 'application',
-              tags: %w[Network Tcp Memcached],
-              vendor: 'Memcached',
-              product: 'Memcached',
+              tags: %w[Network Tcp Zookeeper],
+              vendor: 'Apache',
+              product: 'Zookeeper',
               description: 'match via protocol response string',
               references: [
-                'https://memcached.org/'
+                'https://zookeeper.apache.org/'
               ],
               request_type: :plain,
               protocol: :tcp,
-              request_content: "stats\r\n\r\nquit\r\n",
+              request_content: "envi\r\nquit\r\n",
               # match_type: :content_banner, we don't have a match_type because we can only match whatever we get in the response
-              match_content: /^.*STAT\s.*$/i
+              match_content: /^.*zookeeper.version.*$/i,
+              dynamic_version: lambda { |x|
+                _first_banner_capture(x,  /^.*zookeeper.version=(.*),.*$/i)
+              }
             }
           ]
         end
--- checks/ip/ganglia.rb
+++ checks/ip/ganglia.rb
@@ -1,29 +1,28 @@
 module Intrigue
-    module Ident
-      module IpCheck
-        class Ganglia < Intrigue::Ident::IpCheck::Base
-          def generate_checks
-            [
-              {
-                type: 'fingerprint',
-                category: 'application',
-                tags: %w[Network Tcp Ganglia],
-                vendor: 'Ganglia',
-                product: 'Ganglia',
-                description: 'match via protocol response string',
-                references: [
-                  'http://ganglia.info/'
-                ],
-                request_type: :plain,
-                protocol: :tcp,
-                request_content: "\r\n",
-                # match_type: :content_banner, we don't have a match_type because we can only match whatever we get in the response
-                match_content: /^.*<!DOCTYPE\sGANGLIA_XML.*$/i
-              }
-            ]
-          end
+  module Ident
+    module IpCheck
+      class Ganglia < Intrigue::Ident::IpCheck::Base
+        def generate_checks
+          [
+            {
+              type: 'fingerprint',
+              category: 'application',
+              tags: %w[Network Tcp Ganglia],
+              vendor: 'Ganglia',
+              product: 'Ganglia',
+              description: 'match via protocol response string',
+              references: [
+                'http://ganglia.info/'
+              ],
+              request_type: :plain,
+              protocol: :tcp,
+              request_content: "\r\n",
+              # match_type: :content_banner, we don't have a match_type because we can only match whatever we get in the response
+              match_content: /^.*<!DOCTYPE\sGANGLIA_XML.*$/i
+            }
+          ]
         end
       end
     end
   end
-  
+end
